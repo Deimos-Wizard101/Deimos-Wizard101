e1a5297c31d40dc35318bbc127e0edcd945d332d
nboj <60493251+nboj@users.noreply.github.com>
2024-10-08 13:00:58 -0400

    fixed window reading

--- src/deimoslang/vm.py
+++ src/deimoslang/vm.py
@@ -346,15 +346,17 @@ class VM:
                 assert(type(path) == list)
                 window = await get_window_from_path(client.root_window, path)
                 try:
-                    text = await window.maybe_text()
-                    if text:
-                        return text
+                    if (window):
+                        text = await window.maybe_text()
+                        if text:
+                            return text
                 except (ValueError, MemoryReadError): pass
                 try:
-                    text = await window.read_wide_string_from_offset(616)
-                    return text
+                    if (window):
+                        text = await window.read_wide_string_from_offset(616)
+                        return text
                 except (ValueError, MemoryReadError):
-                    raise Exception(f'Cannot read window.')
+                    raise Exception(f'Cannot read window. {path}')
             case EvalKind.potioncount:
                 return await client.stats.potion_charge()
             case EvalKind.max_potioncount:
